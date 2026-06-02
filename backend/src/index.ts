34f21bcf55299c9d70e808efd90921577336e40f
Atul Kumar Jha <atuljha21542@gmail.com>
2026-06-03 02:42:57 +0530

    A=Initialized main server

---
+++ backend/src/index.ts
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from  'cors';
+import dotenv from  'dotenv';
+import chatRoutes from "./routes/chat.routes";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/chat', chatRoutes);
+app.use('/health', (req, res) => {
+    res.json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`);
+})
